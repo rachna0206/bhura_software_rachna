31c5b4737232e07be70c8e467ebc2e2e22d91f73
rachna0206 <rachna.mewada0206@gmail.com>
2024-12-10 18:35:54 +0530

    contact name & no added in add plotting in company

--- ajaxdata.php
+++ ajaxdata.php
@@ -2820,7 +2820,7 @@ if(isset($_REQUEST['action']))
 		$estate_id=$_REQUEST['estate_id'];
 		$user_id=$_REQUEST['user_id'];
 
-		$stmt = $obj->con1->prepare("SELECT r1.id, r1.raw_data->>'$.post_fields.Firm_Name' as firm_name, r1.raw_data->>'$.post_fields.Factory_Address' as factory_address, r1.raw_data->>'$.post_fields.Mobile_No' as mobile_no, (select stage END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage, (select CASE WHEN stage='lead' THEN 'Positive' WHEN stage='badlead' THEN 'Negative' ELSE 'Existing Client' END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage1 from tbl_tdrawdata r1 where r1.raw_data->'$.post_fields.Taluka'='".$taluka."' and r1.raw_data->'$.post_fields.Area'='".$area."' and r1.raw_data->'$.post_fields.IndustrialEstate'='".$ind_estate."' and JSON_CONTAINS_PATH(raw_data, 'one', '$.plot_details') = 0 and raw_data->'$.post_fields.IndustrialEstate'!='' and r1.id not in (SELECT rawdata_id from pr_company_details)");
+		$stmt = $obj->con1->prepare("SELECT r1.id, r1.raw_data->>'$.post_fields.Firm_Name' as firm_name, r1.raw_data->>'$.post_fields.Factory_Address' as factory_address, r1.raw_data->>'$.post_fields.Mobile_No' as mobile_no,r1.raw_data->>'$.post_fields.Contact_Name' as contact_name, (select stage END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage, (select CASE WHEN stage='lead' THEN 'Positive' WHEN stage='badlead' THEN 'Negative' ELSE 'Existing Client' END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage1 from tbl_tdrawdata r1 where r1.raw_data->'$.post_fields.Taluka'='".$taluka."' and r1.raw_data->'$.post_fields.Area'='".$area."' and r1.raw_data->'$.post_fields.IndustrialEstate'='".$ind_estate."' and JSON_CONTAINS_PATH(raw_data, 'one', '$.plot_details') = 0 and raw_data->'$.post_fields.IndustrialEstate'!='' and r1.id not in (SELECT rawdata_id from pr_company_details)");
 		$stmt->execute();
 		$res = $stmt->get_result();
 		$stmt->close();
@@ -2838,6 +2838,8 @@ if(isset($_REQUEST['action']))
 			            <tr>
 			              <th>Srno</th>
 			              <th>Company Name</th>
+						  <th>Contact Person</th>
+						  <th>Contact No.</th>
 			              <th>Employee Name</th>
 			              <th>Factory Address</th>
 			              <th>Status</th>
@@ -2870,6 +2872,8 @@ if(isset($_REQUEST['action']))
 				$html.='<tr>
 		              <td>'.$i.'</td>
 		              <td>'.$data["firm_name"].'</td>
+					  <td>'.$data["contact_name"].'</td>
+					  <td>'.$data["mobile_no"].'</td>
 		              <td>'.$emp_name.'</td>
 		              <td>'.$data["factory_address"].'</td>
 		              <td>';
@@ -2898,7 +2902,7 @@ if(isset($_REQUEST['action']))
 		$taluka=$_REQUEST['taluka'];
 		$area=$_REQUEST['area'];
 		$user_id=$_REQUEST['user_id'];
-		$stmt = $obj->con1->prepare("SELECT r1.id, r1.raw_data->>'$.post_fields.Firm_Name' as firm_name, r1.raw_data->>'$.post_fields.Factory_Address' as factory_address, r1.raw_data->>'$.post_fields.Mobile_No' as mobile_no, (select stage END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage, (select CASE WHEN stage='lead' THEN 'Positive' WHEN stage='badlead' THEN 'Negative' ELSE 'Existing Client' END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage1 from tbl_tdrawdata r1 where r1.raw_data->'$.post_fields.city'='".$city."' and r1.raw_data->'$.post_fields.Taluka'='".$taluka."' and r1.raw_data->'$.post_fields.Area'='".$area."' and JSON_CONTAINS_PATH(raw_data, 'one', '$.plot_details') = 0 and raw_data->'$.post_fields.IndustrialEstate'='' and id not in (SELECT rawdata_id from pr_company_details)");
+		$stmt = $obj->con1->prepare("SELECT r1.id, r1.raw_data->>'$.post_fields.Firm_Name' as firm_name, r1.raw_data->>'$.post_fields.Factory_Address' as factory_address, r1.raw_data->>'$.post_fields.Mobile_No' as mobile_no,r1.raw_data->>'$.post_fields.Contact_Name' as contact_name, (select stage END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage, (select CASE WHEN stage='lead' THEN 'Positive' WHEN stage='badlead' THEN 'Negative' ELSE 'Existing Client' END from tbl_tdrawassign where inq_id=r1.id order by id desc LIMIT 1) stage1 from tbl_tdrawdata r1 where r1.raw_data->'$.post_fields.city'='".$city."' and r1.raw_data->'$.post_fields.Taluka'='".$taluka."' and r1.raw_data->'$.post_fields.Area'='".$area."' and JSON_CONTAINS_PATH(raw_data, 'one', '$.plot_details') = 0 and raw_data->'$.post_fields.IndustrialEstate'='' and id not in (SELECT rawdata_id from pr_company_details)");
 		$stmt->execute();
 		$res = $stmt->get_result();
 		$stmt->close();
@@ -2916,6 +2920,8 @@ if(isset($_REQUEST['action']))
 			            <tr>
 			              <th>Srno</th>
 			              <th>Company Name</th>
+						  <th>Contact Person</th>
+						  <th>Contact No.</th>
 			              <th>Employee Name</th>
 			              <th>Factory Address</th>
 			              <th>Status</th>
@@ -2948,7 +2954,10 @@ if(isset($_REQUEST['action']))
 				$html.='<tr>
 		              <td>'.$i.'</td>
 		              <td>'.$data["firm_name"].'</td>
+					  <td>'.$data["contact_name"].'</td>
+					  <td>'.$data["mobile_no"].'</td>
 		              <td>'.$emp_name.'</td>
+					  
 		              <td>'.$data["factory_address"].'</td>
 		              <td>';
 		                  $html.=$data["stage1"];
