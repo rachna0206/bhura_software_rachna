f1dde1e3e46673d592872ac751508c01154e0188
rachna0206 <rachna.mewada0206@gmail.com>
2025-01-08 14:06:25 +0530

    update ind estate name menu added in header

--- header.php
+++ header.php
@@ -43,7 +43,7 @@ $stmt_scheme->execute();
 $scheme_result = $stmt_scheme->get_result();
 $stmt_scheme->close();
 
-$adminmenu = array("company_plot_report.php", "assign_estate.php", "unassigned_estate_company.php", "assign_estate_plotting.php", "unassigned_estate_plotting.php", "add_industrial_estate.php", "add_industrial_estate_old.php", "company_entry.php", "estate_plotting_report.php", "estate_status_report.php", "visit_count_report.php", "scheme.php", "stages.php", "pr_file_format.php", "logged_users", "employee_master.php", "company_add_plot.php", "company_add_plot_est.php", "company_add_plot_com.php", "update_status.php", "logged_users.php", "estate_list.php");
+$adminmenu = array("company_plot_report.php", "assign_estate.php", "unassigned_estate_company.php", "assign_estate_plotting.php", "unassigned_estate_plotting.php", "add_industrial_estate.php", "add_industrial_estate_old.php", "company_entry.php", "estate_plotting_report.php", "estate_status_report.php", "visit_count_report.php", "scheme.php", "stages.php", "pr_file_format.php", "logged_users", "employee_master.php", "company_add_plot.php", "company_add_plot_est.php", "company_add_plot_com.php", "update_status.php", "logged_users.php", "estate_list.php","change_ind_estate_name.php");
 
 $processmenu = array("process_gogtp_ir.php", "process_gogtp_pt.php", "process.php");
 /*function checkCompany_rawassign($value)
@@ -301,6 +301,13 @@ function check_for_badlead($value)
                   class="menu-item <?php echo basename($_SERVER["PHP_SELF"]) == "pr_file_format.php" ? "active" : "" ?>">
                   <a href="pr_file_format.php" class="menu-link">
                     <div data-i18n="course">File Format</div>
+                  </a>
+                </li>
+
+                <li
+                  class="menu-item <?php echo basename($_SERVER["PHP_SELF"]) == "change_ind_estate_name.php" ? "active" : "" ?>">
+                  <a href="change_ind_estate_name.php" class="menu-link">
+                    <div data-i18n="course">Update Industrial Estate Name </div>
                   </a>
                 </li>
                 <!-- <li class="menu-item <?php echo basename($_SERVER["PHP_SELF"]) == "scheme.php" ? "active" : "" ?>">
